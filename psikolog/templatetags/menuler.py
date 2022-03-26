d34da7fadebb1df4d1b8f4c37bdc98c82ce3b06e
Muhammed Aydoğan <muhammetay651@gmail.com>
2022-03-27 01:01:03 +0300

    menu sıralama

--- psikolog/templatetags/menuler.py
+++ psikolog/templatetags/menuler.py
@@ -10,6 +10,6 @@ register=template.Library()
 @register.simple_tag
 def get_all_menus(request):
     if request.user.is_authenticated:
-        return topMenuModel.objects.filter(userType="authUser").all()
-    return topMenuModel.objects.filter(userType="anonim").all()
+        return topMenuModel.objects.filter(userType="authUser").all().order_by("menuSira")
+    return topMenuModel.objects.filter(userType="anonim").all().order_by("menuSira")
 
