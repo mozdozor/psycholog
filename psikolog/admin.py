28c11164e486ba563e254c137020daad75a5d347
Muhammed Aydoğan <muhammetay651@gmail.com>
2022-04-05 16:10:42 +0300

    csrf trusted origins base a eklendi

--- psikolog/admin.py
+++ psikolog/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 
 from psikolog.models import CommentModel, CustomUserModel, billingCourseModel, favouriteCourseModel, sliderModel
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-
-
-admin.site.register(CustomUserModel)
 
 admin.site.register(CommentModel)
 
@@ -13,3 +11,11 @@ admin.site.register(billingCourseModel)
 
 admin.site.register(favouriteCourseModel)
 admin.site.register(sliderModel)
+
+
+
+@admin.register(CustomUserModel)
+class CustomUserAdmin(UserAdmin):
+    readonly_fields = [
+        'date_joined',
+    ]
