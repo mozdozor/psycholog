4f28aecb81ae59349dc6b48d55ba8826947b9cfa
Muhammed Aydoğan <muhammetay651@gmail.com>
2022-04-09 09:50:51 +0300

    mesafeli satış sözleşmesi eklendi

--- psikolog/views.py
+++ psikolog/views.py
@@ -600,8 +600,8 @@ def callback(request):
 
     if post['status'] == 'success':  # Ödeme Onaylandı
 
-        orderModel.objects.create(course=currentCourse,user=currentUser,merchant_oid=post['merchant_oid'],status="yes")
-
+      #  orderModel.objects.create(course=currentCourse,user=currentUser,merchant_oid=post['merchant_oid'],status="yes")
+        return HttpResponse(str(currentCourse.title))
         """
         BURADA YAPILMASI GEREKENLER
         1) Siparişi onaylayın.
